21ec2ee7b0383a35774e85ade09a3736fe945af0
bmchristensen <bmchristensen@gmail.com>
2019-04-15 09:36:51 -0700

    Implement let method to reduce duplication of class instantiation

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper.rb'
 
 RSpec.describe Project do
+  let(:project) { Project.new }
+  let(:ingredient) { Ingredient.new }
+
   it 'considers an empty project to be a draft' do
-    project = Project.new
     expect(project.draft).to be_truthy
   end
 
   it 'allows an ingredient to be added' do
-    project = Project.new
-    ingredient = Ingredient.new
     project.ingredients << ingredient
     expect(project.total_size).to eq(1)
   end
