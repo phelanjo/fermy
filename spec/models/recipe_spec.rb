f5572f64d8cc514927e1a1669bca1fb4d661f63c
bmchristensen <bmchristensen@gmail.com>
2019-04-15 07:32:41 -0700

    Add test for add ingredient method

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Recipe do
     recipe = Recipe.new
     expect(project.draft).to be_truthy
   end
+
+  it 'allows an ingredient to be added' do
+    recipe = Recipe.new
+    recipe.add
+    expect(recipe.size).to eq(1)
+  end
 end
