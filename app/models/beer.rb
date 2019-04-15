18fbd05d745777231b8785aadb93028073ab52c6
xDOASx <benesbit@yahoo.com>
2019-04-15 00:28:52 -0700

    Add let to beer_spec.rb to reduce redundency

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,7 +4,11 @@ class Beer
   attr_accessor :beer_type
 
   def initialize(options = {})
-    @type = options[:beer_type] || default_beer_type
+    @beer_type = options[:beer_type] || default_beer_type
+  end
+
+  def exists?
+    true
   end
 
   def beer_type?
