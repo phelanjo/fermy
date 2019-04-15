6f365786a7a6f6ef82717f3b8157639f6de31e91
xDOASx <benesbit@yahoo.com>
2019-04-14 21:21:44 -0700

    Add test for default beer type in beer_spec.rb

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Beer do
   it "can be created" do
     beer = Beer.new
   end
+
+  it "is type Generic unless specified" do
+    beer = Beer.new
+    expect(beer.type?).to be_eq("Generic")
+  end
 end
