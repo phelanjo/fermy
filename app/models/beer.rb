436293324c634c6a0e16000da2c8d036a7670d05
xDOASx <benesbit@yahoo.com>
2019-04-24 11:53:02 -0700

    Edit

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,12 +1,6 @@
 # beer.rb
 
 class Beer < ApplicationRecord
-  # attr_accessor :beer_type, :name
-
-  # def initialize(options = {})
-  #   @beer_type = options[:beer_type] || default_beer_type
-  #   @name = options[:name] || nil
-  # end
 
   def exists?
     true
@@ -19,9 +13,5 @@ class Beer < ApplicationRecord
   def beer_type?
     beer_type
   end
-
-  # def default_beer_type
-  #   'Generic'
-  # end
 
 end
