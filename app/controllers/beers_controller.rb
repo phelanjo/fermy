ddfe326acce8cc65a4e568c12e3fa25a9ff7792b
xDOASx <benesbit@yahoo.com>
2019-04-24 11:37:08 -0700

    Add create method to beers_controller

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -2,4 +2,12 @@ class BeersController < ApplicationController
   def new
     @beer = Beer.new
   end
+
+  def create
+    @workflow = CreatesBeer.new(
+      name: params[:beer][:name],
+      beer_type: params[:beer][:beer_type])
+    @workflow.create
+    redirect_to beers_path
+  end
 end
