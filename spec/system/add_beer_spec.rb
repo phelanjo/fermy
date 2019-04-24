1c135fa26e29245fb44478354568b2f81b0d1177
xDOASx <benesbit@yahoo.com>
2019-04-24 12:00:42 -0700

    Add system test for beer with name and type - fails: doesn't correctly add beer type

--- spec/system/add_beer_spec.rb
+++ spec/system/add_beer_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe "adding a beer", type: :system do
       expect(page).to have_content("Fake Beer")
       expect(page).to have_content("Generic")
   end
+
+  it "allows a user to create a beer with a name and beer type" do
+    visit new_beer_path
+    fill_in "Name", with: "Fake Beer2"
+    fill_in "beer_type", with: "Fake IPA"
+    click_on("Create Beer")
+    visit beers_path
+    expect(page).to have_content("Fake Beer2")
+    expect(page).to have_content("Fake IPA")
+  end
 end
