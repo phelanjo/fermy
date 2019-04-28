2d156284623a56f75f483283555ab071747f8cda
bmchristensen <bmchristensen@gmail.com>
2019-04-27 23:00:03 -0700

    Update methods to initialize project with a name, references #6

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,13 @@
 class Project < ApplicationRecord
   has_many :ingredients, dependent: :destroy
+  validates :name, presence: true
 
   def draft
     ingredients.empty?
+  end
+
+  def name?
+    name
   end
 
   def total_size
--- app/workflows/creates_project.rb
+++ app/workflows/creates_project.rb
@@ -3,12 +3,24 @@ class CreatesProject
 
   def initialize(name: '', ingredients_string: '')
     @name = name
-    @ingredient_string = ingredients_string
+    @ingredients_string = ingredients_string
   end
 
   def build
     self.project = Project.new(name: name)
     project.ingredients = convert_string_to_ingredients
     project
+  end
+
+  def create
+    build
+    project.save
+  end
+
+  def convert_string_to_ingredients
+    ingredients_string.split('\n').map do |one_ingredient|
+      name, amount_string = one_ingredient.split(':')
+      Ingredient.new(name: name, amount: amount_as_integer(amount_string))
+    end
   end
 end
