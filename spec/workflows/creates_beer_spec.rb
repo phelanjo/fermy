1bbe6e5fcbc6ae9b090961edf8100d88668dae88
xDOASx <benesbit@yahoo.com>
2019-04-27 17:03:32 -0700

    Add failure case test

--- spec/workflows/creates_beer_spec.rb
+++ spec/workflows/creates_beer_spec.rb
@@ -2,11 +2,18 @@ require "rails_helper"
 
 RSpec.describe CreatesBeer do
   describe "initialization" do
-
     it "creates a beer given a name" do
       creator = CreatesBeer.new(name: "Fake_Beer_Numba_One")
       creator.build
       expect(creator.beer.name).to eq("Fake_Beer_Numba_One")
+    end
+  end
+
+  describe "failure cases" do
+    it "fails when trying to save a beer with no name" do
+      creator = CreatesBeer.new(name: "", beer_type: "")
+      creator.create
+      expect(creator).not_to be_a_success
     end
   end
 
