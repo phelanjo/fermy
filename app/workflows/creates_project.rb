acb9d8f2c8f34bae1ae31272e8c2f0687ac8e5e1
bmchristensen <bmchristensen@gmail.com>
2019-04-27 22:13:36 -0700

    Update initialize and accessor, add build method, references #6

--- app/workflows/creates_project.rb
+++ app/workflows/creates_project.rb
@@ -1,11 +1,13 @@
 class CreatesProject
-  attr_accessor :name, :project
+  attr_accessor :name, :project, :ingredients_string
 
-  def initialize(name: '')
+  def initialize(name: '', ingredients_string: '')
     @name = name
+    @ingredient_string = ingredients_string
   end
 
   def build
     self.project = Project.new(name: name)
+    project.ingredients = convert_string_to_ingredients
+    project
   end
-end
