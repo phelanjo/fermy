19763284ea15c70d842e0f4fd4737c5f14d95600
bmchristensen <bmchristensen@gmail.com>
2019-04-28 08:53:38 -0700

    Refactor tests to single line and utilize has selector, references #6

--- spec/system/add_project_spec.rb
+++ spec/system/add_project_spec.rb
@@ -4,11 +4,15 @@ RSpec.describe 'adding a new fermentation project', type: :system do
   it 'allows a user to create a project with ingredients' do
     visit new_project_path
     fill_in 'Name', with: 'Project Kimchi'
-    fill_in 'Ingredients', with: 'Daikon\nFish Sauce'
+    fill_in 'Ingredients', with: 'Daikon:2\nFish Sauce'
     click_on('Create Project')
     visit projects_path
-    expect(page).to have_content('Project Kimchi')
-    expect(page).to have_content('Daikon')
-    expect(page).to have_content('Fish Sauce')
+    @project = Project.find_by(name: 'Project Kimchi')
+    expect(page).to have_selector(
+      "#project_#{@project.id} .name", text: 'Project Kimchi'
+    )
+    expect(page).to have_selector(
+      "#project_#{@project.id} .total-size", text: '2'
+    )
   end
 end
--- spec/workflows/creates_project_spec.rb
+++ spec/workflows/creates_project_spec.rb
@@ -1,35 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe CreatesProject do
+  let(:creator) { CreatesProject.new(
+    name: 'Project Kimchi',
+    ingredients_string: ingredients_string) }
+
   describe 'initialization' do
+    let(:ingredients_string) {''}
     it 'creates a project given a name' do
-      creator = CreatesProject.new(name: 'Project Kimchi')
       creator.build
       expect(creator.project.name).to eq('Project Kimchi')
     end
   end
 
   describe 'ingredients string parsing' do
-    it 'handles an empty string' do
-      creator = CreatesProject.new(
-        name: 'Project Kimchi',
-        ingredients_string: ''
-      )
-      ingredients = creator.convert_string_to_ingredients
-      expect(ingredients).to be_empty
+    let(:ingredients) { creator.convert_string_to_ingredients }
+
+    describe 'with an empty string' do
+      let(:ingredients_string) { '' }
+      specify { expect(ingredients).to be_empty }
     end
 
-    it 'handles a single string' do
-      creator = CreatesProject.new(
-        name: 'Project Kimchi',
-        ingredients_string: 'Fish Sauce'
-      )
-      ingredients = creator.convert_string_to_ingredients
-      expect(ingredients.size).to eq(1)
-      expect(ingredients.first).to have_attributes(
-        name: 'Fish Sauce',
-        amount: 1
-      )
+    describe 'with a single string' do
+      let(:ingredients_string) { 'Fish Sauce' }
+      specify { expect(ingredients.size).to eq(1) }
+      specify { expect(ingredients.first).to have_attributes(
+        name: 'Fish Sauce', amount: 1) }
     end
   end
 end
