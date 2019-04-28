ba3b578ec65527a17daaa5b6bf3581e56d5de7c5
bmchristensen <bmchristensen@gmail.com>
2019-04-27 22:20:52 -0700

    Update ingredients_string naming and formatting, references #6

--- spec/workflows/creates_project_spec.rb
+++ spec/workflows/creates_project_spec.rb
@@ -10,14 +10,16 @@ RSpec.describe CreatesProject do
 
   describe 'ingredients string parsing' do
     it 'handles an empty string' do
-      creator = CreatesProject.new(name: 'Project Kimchi', ingredient_string: '')
+      creator = CreatesProject.new(
+        name: 'Project Kimchi', ingredients_string: ''
+      )
       ingredients = creator.convert_string_to_ingredients
       expect(ingredients).to be_empty
     end
 
     it 'handles a single string' do
       creator = CreatesProject.new(
-        name: 'Project Kimchi', ingredient_string: 'Fish Sauce'
+        name: 'Project Kimchi', ingredients_string: 'Fish Sauce'
       )
       ingredients = creator.convert_string_to_ingredients
       expect(ingredients.size).to eq(1)
