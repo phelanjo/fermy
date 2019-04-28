ef7cd6b78058e8ca20d49f83ce8f25217a6152bb
bmchristensen <bmchristensen@gmail.com>
2019-04-28 12:59:24 -0700

    Add additional use test cases, references #6

--- spec/workflows/creates_project_spec.rb
+++ spec/workflows/creates_project_spec.rb
@@ -28,6 +28,21 @@ RSpec.describe CreatesProject do
         name: 'Fish Sauce', amount: 1) }
     end
 
+    describe 'handles an ingredient list with an amount' do
+      let(:ingredients_string) { 'Daikon:5' }
+      specify { expect(ingredients.size).to eq(1) }
+      specify { expect(ingredients.first).to have_attributes(
+        name: 'Daikon', amount: 5
+      )}
+    end
+
+    describe 'attaches ingredients to the project' do
+      let(:ingredients_string) {'Daikon\nFish Sauce'}
+      before(:example) { creator.create }
+      specify { expect(creator.project.ingredients.size).to eq(2) }
+      specify { expect(creator.project).not_to be_a_new_record }
+    end
+
     describe 'failure cases' do
       it 'fails when trying to save a project with no name' do
         creator = CreatesProject.new(name: '', ingredients_string: '')
