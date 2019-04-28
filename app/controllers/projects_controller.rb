950f1b104d7c21d19a9febd31d9fb45206049243
bmchristensen <bmchristensen@gmail.com>
2019-04-27 22:38:09 -0700

    Add create method, references #6

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,13 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def create
+    @workflow = CreatesProject.new(
+      name: params[:project][:ingredients],
+      ingredients_string: params[:project][:ingredients]
+    )
+    @workflow.create
+    redirect_to projects_path
+  end
 end
