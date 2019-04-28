7d7a3d6dc7677384d6b6520cfbe58367c8a37e07
bmchristensen <bmchristensen@gmail.com>
2019-04-27 22:04:39 -0700

    Update workflows spec with init and string handling tests, references #6

--- spec/workflows/creates_project_spec.rb
+++ spec/workflows/creates_project_spec.rb
@@ -1,9 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe CreatesProject do
-  it 'creates a project givena  name' do
-    creator = CreatesProject.new(name: 'Project Kimchi')
-    creator.build
-    expect(creator.project.name).to eq('Project Runway')
+  describe 'initialization' do
+    it 'creates a project given a name' do
+      creator = CreatesProject.new(name: 'Project Kimchi')
+      expect(creator.project.name).to eq('Project Runway')
+    end
+  end
+
+  describe 'ingredients string parsing' do 
+    it 'handles an empty string' do
+      creator = CreatesProject.new(name: 'Project Kimchi', ingredient_string: '')
+      ingredients = creator.convert_string_to_ingredients
+      expect(ingredients).to be_empty
+    end
+
+    it 'handles a single string' do
+      creator = CreatesProject.new(name: 'Project Kimchi', ingredient_string: 'Fish Sauce')
+      ingredients = creator.convert_string_to_ingredients
+      expect(ingredients.size).to eq(1)
+      expect(ingredients.first).to have_attributes(name: 'Fish Sauce', size: 1)
+    end
   end
 end
