65c97377e09f36f3db382d05bacdcd2ad1dd1700
bmchristensen <bmchristensen@gmail.com>
2019-05-04 12:11:08 -0700

    Refactor ingredient unit tests, assert valid and invalid, references 9

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -2,24 +2,21 @@ require 'rails_helper.rb'
 
 RSpec.describe Ingredient do
   let(:ingredient) { Ingredient.new }
+  let(:valid_ingredient) { Ingredient.new(name: 'hops') }
 
   it 'does not have an amount for a new ingredient' do
     expect(ingredient.amount).to be_nil
   end
 
-  it 'must have a name' do
+  it 'is not valid without a user specified name' do
     expect(ingredient).to be_invalid
   end
 
-  describe 'names' do
-    let(:project) { Project.new(name: 'test') }
+  it 'is valid with a user specified name' do
+    expect(valid_ingredient).to be_valid
+  end
 
-    it 'is valid with user given name' do
-      expect(ingredient).to be_valid
-    end
-
-    it 'allow a user to provide the name' do
-      expect(ingredient.name).to eq('hops')
-    end
+  it 'allows a user to provide a name' do
+    expect(valid_ingredient.name).to eq('hops')
   end
 end
