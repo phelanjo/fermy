158c5a6241f7f399fdecb68450c4eb10542e93cb
bmchristensen <bmchristensen@gmail.com>
2019-05-04 12:50:04 -0700

    Create migration for adding a description feature to ingredients, references #9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_175529) do
+ActiveRecord::Schema.define(version: 2019_05_04_194839) do
 
   create_table "beers", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_05_01_175529) do
     t.integer "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["project_id"], name: "index_ingredients_on_project_id"
   end
 
