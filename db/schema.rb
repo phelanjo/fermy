e4eb2ebb500359bc680fc965f7445171d2bb9c05
bmchristensen <bmchristensen@gmail.com>
2019-05-04 13:11:40 -0700

    Update schema for unit of measure, references #9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_04_194839) do
+ActiveRecord::Schema.define(version: 2019_05_04_201011) do
 
   create_table "beers", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_05_04_194839) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
+    t.string "unit"
     t.index ["project_id"], name: "index_ingredients_on_project_id"
   end
 
