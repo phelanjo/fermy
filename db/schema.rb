3a57cdaba3d5b95fbe588550383f39b2f1f8b360
bmchristensen <bmchristensen@gmail.com>
2019-05-04 16:24:01 -0700

    Create migration for changing amount to decimal type, references #9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_04_201011) do
+ActiveRecord::Schema.define(version: 2019_05_04_231854) do
 
   create_table "beers", force: :cascade do |t|
     t.string "name"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2019_05_04_201011) do
   create_table "ingredients", force: :cascade do |t|
     t.integer "project_id"
     t.string "name"
-    t.integer "amount"
+    t.decimal "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
