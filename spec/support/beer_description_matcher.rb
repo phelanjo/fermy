e303587a97929c9ae6d052b6e218092a4a10f799
xDOASx <benesbit@yahoo.com>
2019-05-05 13:32:02 -0700

    Override RSpec description for custom matcher for beer description, references #9

--- spec/support/beer_description_matcher.rb
+++ spec/support/beer_description_matcher.rb
@@ -2,4 +2,8 @@ RSpec::Matchers.define :have_beer_description do |expected|
   match do |actual|
     actual.description == expected
   end
+
+  description do
+    "have a beer with a description #{expected}"
+  end
 end
