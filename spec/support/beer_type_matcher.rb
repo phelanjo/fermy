d6c9698a1b5f67bd410d2d1c89b2946bd0899d5c
xDOASx <benesbit@yahoo.com>
2019-05-05 13:20:46 -0700

    Override RSpec failure message for custom matcher, references #9

--- spec/support/beer_type_matcher.rb
+++ spec/support/beer_type_matcher.rb
@@ -6,4 +6,8 @@ RSpec::Matchers.define :be_of_beer_type do |expected|
   description do
     "have a beer type of #{expected}"
   end
+
+  failure_message do |actual|
+    "expected project #{actual.beer_type} to be beer type #{expect}, was #{actual}"
+  end
 end
