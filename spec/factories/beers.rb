ba80107fd8b4d1ba5611a307aa310b99841537ae
xDOASx <benesbit@yahoo.com>
2019-05-14 21:31:35 -0700

    Stub out basic factory for beers, references 11

---
+++ spec/factories/beers.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :beer do
+    name { "Fake Factory Beer" }
+    type { "Fake Factory Beer Type" }
+    description { "Some Fake Factory Beer Description" }
+  end
+end
