8c865d5a8f38029b9a15b04a310f98689595ca2f
bmchristensen <bmchristensen@gmail.com>
2019-05-19 14:25:36 -0700

    Create additional unit test for total_size method, references #14
    This unit test will support the dependency injection into the total_size method in a more streamlined fashion.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper.rb'
 RSpec.describe Project do
   let(:project) { Project.new }
   let(:ingredient) { Ingredient.new }
+  let(:fake_hops) { FactoryBot.build_stubbed(:ingredient, name: 'hops') }
+  let(:fake_barley) { FactoryBot.build_stubbed(:ingredient, name: 'barley') }
+  let(:fake_water) { FactoryBot.build_stubbed(:ingredient, name: 'water') }
 
   it 'considers an empty project to be a draft' do
     expect(project.draft).to be_truthy
@@ -14,5 +17,12 @@ RSpec.describe Project do
     ingredient.name = 'TEST'
     project.add_ingredient(ingredient)
     expect(project.total_size).to eq(1)
+  end
+
+  it 'calculates the number of ingredients' do
+    project.add_ingredient(fake_hops)
+    project.add_ingredient(fake_barley)
+    project.add_ingredient(fake_water)
+    expect(project.total_size).to eq(3)
   end
 end
